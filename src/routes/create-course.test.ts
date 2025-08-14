275a3a3e58f27a5ac958c7767ccc651027764fee
Bruno23 <monteiro_100@outlook.pt>
2025-08-14 18:08:37 -0300

    feat: exclusao do arquivo seed da pasta test

--- src/routes/create-course.test.ts
+++ src/routes/create-course.test.ts
@@ -1,18 +1,20 @@
-import { test, expect } from 'vitest'
-import request from 'supertest'
-import { server } from '../app.ts'
-import { faker } from '@faker-js/faker'
+import { test, expect } from "vitest";
+import request from "supertest";
+import { server } from "../app.ts";
+import { faker } from "@faker-js/faker";
 
-test('create a course', async () => {
-  await server.ready()
+test("create a course", async () => {
+  await server.ready();
 
   const response = await request(server.server)
-    .post('/courses')
-    .set('Content-Type', 'application/json')
+    .post("/courses")
+    .set("Content-Type", "application/json")
     .send({ title: faker.lorem.words(4) })
+    .send({ description: faker.lorem.paragraph(4) })
+    .expect("Content-Type", /json/)
+    .expect(201);
 
-  expect(response.status).toEqual(201)
   expect(response.body).toEqual({
     courseId: expect.any(String),
-  })
-})
+  });
+});
--- src/routes/get-course-by-id.test.ts
+++ src/routes/get-course-by-id.test.ts
@@ -1,31 +1,31 @@
-import { test, expect } from 'vitest'
-import request from 'supertest'
-import { server } from '../app.ts'
-import { makeCourse } from '../tests/factories/make-course.ts'
+import { test, expect } from "vitest";
+import request from "supertest";
+import { server } from "../app.ts";
+import { makeCourse } from "../tests/factories/make-course.ts";
 
-test('get course by id', async () => {
-  await server.ready()
+test("get course by id", async () => {
+  await server.ready();
 
-  const course = await makeCourse()
+  const course = await makeCourse();
 
-  const response = await request(server.server)
-    .get(`/courses/${course.id}`)
+  const response = await request(server.server).get(`/courses/${course.id}`);
 
-  expect(response.status).toEqual(200)
+  expect(response.status).toEqual(200);
   expect(response.body).toEqual({
     course: {
       id: expect.any(String),
       title: expect.any(String),
-      description: null,
-    }
-  })
-})
+      description: expect.any(String),
+    },
+  });
+});
 
-test('return 404 for non existing courses', async () => {
-  await server.ready()
+test("return 404 for non existing courses", async () => {
+  await server.ready();
 
-  const response = await request(server.server)
-    .get(`/courses/CBA2E131-C83C-471A-9DAC-4F4A84B55476`)
+  const response = await request(server.server).get(
+    `/courses/CBA2E131-C83C-471A-9DAC-4F4A84B55476`
+  );
 
-  expect(response.status).toEqual(404)
-})
+  expect(response.status).toEqual(404);
+});
