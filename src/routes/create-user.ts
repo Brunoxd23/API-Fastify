76a71c3ac6cc5071203f442df7c440b5e3494b8f
Seu Nome <seu@email.com>
2025-08-26 23:49:58 -0300

    feat(user): implement user creation and query routes

---
+++ src/routes/create-user.ts
@@ -0,0 +1,41 @@
+// src/routes/create-user.ts
+import type { FastifyPluginAsync } from "fastify";
+import { z } from "zod";
+import { db } from "../database/client.ts";
+import { users } from "../database/schema.ts";
+import { hash } from "argon2";
+
+export const createUserRoute: FastifyPluginAsync = async (server) => {
+  server.post(
+    "/users",
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(3),
+          email: z.string().email(),
+          password: z.string().min(6),
+          role: z.enum(["student", "manager"]).default("student"),
+        }),
+        response: {
+          201: z.object({ userId: z.string().uuid() }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password, role } = request.body as any;
+      const hashedPassword = await hash(password);
+
+      const [user] = await db
+        .insert(users)
+        .values({
+          name,
+          email,
+          password: hashedPassword,
+          role,
+        })
+        .returning();
+
+      return reply.status(201).send({ userId: user.id });
+    }
+  );
+};
